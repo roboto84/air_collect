182d07843ef9a66176da6ec826522ba4f06bc680
roboto84 <7658704+roboto84@users.noreply.github.com>
2020-12-05 11:15:30 -0500

    refactor(init): split original project to separate concerns
    This project was the original data collector for project
    'Baronerd' which was renamed to project 'Air' within this work.
    Along with the rename, the new project was split up into
    multiple git repositories in order to separate development and
    running environment concerns. This project is only concerned
    with the data collection portion of 'Air', leaving plotting of
    the data and other functionality to the other repos within
    this project.

---
+++ air_collect/bin/AirSettings.py
@@ -0,0 +1,17 @@
+#  Configuration for different plotting perspectives
+
+File_Settings = {
+    'live_data': {
+        'data_file': 'data/liveData.csv',
+        'diff_file': 'data/differentials/liveDataDiff.csv',
+        'diff2_file': 'data/differentials/liveData2ndOrderDiffFile.csv'
+    },
+    'next_48_hours': {
+        'data_file': 'data/next48Data.csv',
+        'diff_file': 'data/differentials/next48HrsDataDiffFile.csv'
+    },
+    'next_7_days': {
+        'data_file': 'data/next7DaysData.csv',
+        'diff_file': 'data/differentials/next7DaysDataDiffFile.csv'
+    }
+}
