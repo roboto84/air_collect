21ddb99aaaf02e372daba9c9102c10fc669b7145
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-01-11 15:58:56 -0500

    style(typings): add basic type annotations

--- air_collect/air_collect.py
+++ air_collect/air_collect.py
@@ -4,9 +4,11 @@ import requests
 import json
 import ntpath
 import os
+import sys
 import logging.config
 import numpy as np
 import pandas as pd
+from typing import Any, NoReturn, List
 from dotenv import load_dotenv
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -15,38 +17,38 @@ from bin.air_settings import file_settings
 
 class AirCollect:
 
-    def __init__(self, logging_object, home_path, api_key, location_lat_long, query_interval, trim_interval,
-                 num_of_readings):
-        self.home_path = home_path
-        self.logger = logging_object.getLogger(type(self).__name__)
+    def __init__(self, logging_object: Any, home_path: str, api_key: str, location_lat_long: str,
+                 query_interval: int, trim_interval: int, num_of_readings: int):
+        self.home_path: str = home_path
+        self.logger: Any = logging_object.getLogger(type(self).__name__)
         self.logger.setLevel(logging.INFO)
 
-        self.live_data_file = self.get_full_file_path(file_settings['live_data']['data_file'])
-        self.next_48_hours_data_file = self.get_full_file_path(file_settings['next_48_hours']['data_file'])
-        self.next_7_days_data_file = self.get_full_file_path(file_settings['next_7_days']['data_file'])
+        self.live_data_file: str = self.get_full_file_path(file_settings['live_data']['data_file'])
+        self.next_48_hours_data_file: str = self.get_full_file_path(file_settings['next_48_hours']['data_file'])
+        self.next_7_days_data_file: str = self.get_full_file_path(file_settings['next_7_days']['data_file'])
 
-        self.live_data_diff_file = self.get_full_file_path(file_settings['live_data']['diff_file'])
-        self.live_data_2nd_order_diff_file = self.get_full_file_path(file_settings['live_data']['diff2_file'])
-        self.next_48_hours_data_diff_file = self.get_full_file_path(file_settings['next_48_hours']['diff_file'])
-        self.next_7_days_data_diff_file = self.get_full_file_path(file_settings['next_7_days']['diff_file'])
+        self.live_data_diff_file: str = self.get_full_file_path(file_settings['live_data']['diff_file'])
+        self.live_data_2nd_order_diff_file: str = self.get_full_file_path(file_settings['live_data']['diff2_file'])
+        self.next_48_hours_data_diff_file: str = self.get_full_file_path(file_settings['next_48_hours']['diff_file'])
+        self.next_7_days_data_diff_file: str = self.get_full_file_path(file_settings['next_7_days']['diff_file'])
 
-        self.dark_sky_api_url = 'https://api.darksky.net/forecast/{}/{}'.format(api_key, location_lat_long)
-        self.query_api_interval = query_interval
-        self.trim_date_interval = trim_interval
-        self.num_of_live_readings = num_of_readings
+        self.dark_sky_api_url: str = 'https://api.darksky.net/forecast/{}/{}'.format(api_key, location_lat_long)
+        self.query_api_interval: int = query_interval
+        self.trim_date_interval: int = trim_interval
+        self.num_of_live_readings: int = num_of_readings
 
-    def run_metronome(self):
+    def run_metronome(self) -> NoReturn:
         try:
-            scheduler = BlockingScheduler()
+            scheduler: BlockingScheduler = BlockingScheduler()
             scheduler.add_job(self.query_dark_sky_api, 'interval', seconds=self.query_api_interval)
             scheduler.add_job(self.trim_data, 'interval', seconds=self.trim_date_interval)
             scheduler.start()
         except KeyboardInterrupt:
             self.logger.warning('Received a KeyboardInterrupt... exiting process')
-            exit()
+            sys.exit()
 
     @staticmethod
-    def standard_time(unix_time, time_type):
+    def standard_time(unix_time: int, time_type: str) -> str:
         if time_type == 'daily':
             return datetime.fromtimestamp(unix_time).strftime('%m/%d')
         elif time_type == 'hourly':
@@ -54,33 +56,33 @@ class AirCollect:
         else:
             return datetime.fromtimestamp(unix_time).strftime('%m/%d %H:%M')
 
-    def get_full_file_path(self, relative_file_path):
+    def get_full_file_path(self, relative_file_path: str) -> str:
         return os.path.join(self.home_path, relative_file_path)
 
     @staticmethod
-    def transform_to_inhg_pressure(mbar_pressure):
+    def transform_to_inhg_pressure(mbar_pressure: float) -> float:
         return round(mbar_pressure * 0.029530, 4)
 
-    def delta(self, api_data):
-        pressure_array = []
+    def delta(self, api_data: List[dict]) -> List[float]:
+        pressure_array: List[float] = []
         for tuple_data in api_data:
             pressure_array.append(self.transform_to_inhg_pressure(tuple_data.get('pressure')))
         return np.round(np.diff(pressure_array), 4)
 
     @staticmethod
-    def write_diff(diff_array, file_name):
+    def write_diff(diff_array: List[float], file_name: str):
         with open(file_name, 'w') as diffData:
             diffData.write('Pressure Delta,Time Interval\n')
             for pos, diff in enumerate(diff_array):
                 diffData.write('%s,%s\n' % (diff, pos))
 
     @staticmethod
-    def write_live_data(obj_logger, pressure, current_time, file_name):
+    def write_live_data(obj_logger: Any, pressure: float, current_time: str, file_name: str):
         obj_logger.info(f'{pressure} inHg')
         with open(file_name, 'a') as liveData:
             liveData.write('%s,%s\n' % (pressure, current_time))
 
-    def write_next_data(self, next_api_data, file_name, time_type):
+    def write_next_data(self, next_api_data: List[dict], file_name: str, time_type: str):
         with open(file_name, 'w') as nextData:
             nextData.write('Pressure,Time\n')
             for timeInterval in next_api_data:
@@ -88,57 +90,57 @@ class AirCollect:
                     self.transform_to_inhg_pressure(timeInterval.get('pressure')),
                     self.standard_time(timeInterval.get('time'), time_type)))
 
-    def process_data_feeds(self):
-        data = pd.read_csv(self.live_data_file)
+    def process_live_data_diffs(self) -> NoReturn:
+        data: dict = pd.read_csv(self.live_data_file)
         self.write_diff(np.round(np.diff(data['Pressure']), 4), self.live_data_diff_file)
         self.write_diff(np.round(np.diff(data['Pressure'], n=2), 4), self.live_data_2nd_order_diff_file)
 
-    def query_dark_sky_api(self):
-        response = requests.get(self.dark_sky_api_url)
+    def query_dark_sky_api(self) -> NoReturn:
+        response: Any = requests.get(self.dark_sky_api_url)
         if response:
-            weather = json.loads(response.text)
-            currently = weather.get('currently')
+            weather: dict = json.loads(response.text)
+            currently: dict = weather.get('currently')
             self.write_live_data(self.logger, self.transform_to_inhg_pressure(currently.get('pressure')),
                                  self.standard_time(currently.get('time'), 'live'), self.live_data_file)
             self.write_next_data(weather.get('hourly').get('data'), self.next_48_hours_data_file, 'hourly')
             self.write_next_data(weather.get('daily').get('data'), self.next_7_days_data_file, 'daily')
             self.write_diff(self.delta(weather.get('hourly').get('data')), self.next_48_hours_data_diff_file)
             self.write_diff(self.delta(weather.get('daily').get('data')), self.next_7_days_data_diff_file)
-            self.process_data_feeds()
+            self.process_live_data_diffs()
         else:
             self.logger.error(f'API Status code: {response.status_code}')
 
-    def trim_data(self):
+    def trim_data(self) -> NoReturn:
         # check if data file has reach the configured live data limit
         self.logger.debug('trim data running')
         with open(self.live_data_file, 'r') as data:
-            num_lines = sum(1 for line in data)
+            num_lines: int = sum(1 for line in data)
 
         if num_lines - 1 > self.num_of_live_readings:
             with open(self.live_data_file, 'r') as infile:
-                lines = infile.readlines()
+                lines: List[str] = infile.readlines()
 
             with open(self.live_data_file, 'w') as outfile:
                 for pos, line in enumerate(lines):
                     if pos != 1:
                         outfile.write(line)
 
-            self.process_data_feeds()
+            self.process_live_data_diffs()
 
 
 if __name__ == '__main__':
-    HOME_PATH = ntpath.dirname(__file__)
+    HOME_PATH: str = ntpath.dirname(__file__)
     logging.config.fileConfig(fname=os.path.join(HOME_PATH, 'bin/logging.conf'), disable_existing_loggers=False)
-    logger = logging.getLogger(__name__)
+    logger: Any = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
     try:
         load_dotenv()
-        DARK_SKY_API_KEY = os.getenv('DARK_SKY_API_KEY')
-        QUERY_API_INTERVAL = int(os.getenv('QUERY_API_INTERVAL'))
-        TRIM_DATA_INTERVAL = int(QUERY_API_INTERVAL / 3)
-        NUM_OF_LIVE_READINGS = int(os.getenv('NUM_OF_LIVE_READINGS'))
-        COORDINATES_LAT_LONG = os.getenv('COORDINATES_LAT_LONG')
+        DARK_SKY_API_KEY: str = os.getenv('DARK_SKY_API_KEY')
+        QUERY_API_INTERVAL: int = int(os.getenv('QUERY_API_INTERVAL'))
+        TRIM_DATA_INTERVAL: int = int(QUERY_API_INTERVAL / 3)
+        NUM_OF_LIVE_READINGS: int = int(os.getenv('NUM_OF_LIVE_READINGS'))
+        COORDINATES_LAT_LONG: str = os.getenv('COORDINATES_LAT_LONG')
 
         print('\nBarometric data collector will run every {} seconds for coordinates {}:'.format(QUERY_API_INTERVAL,
                                                                                                  COORDINATES_LAT_LONG))
