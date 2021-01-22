48cacf93eec26fa084890667efc84a5dd9368f1b
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-01-22 12:07:20 -0500

    refactor(data): relocate where data is written

--- air_collect/bin/air_settings.py
+++ air_collect/bin/air_settings.py
@@ -2,16 +2,16 @@
 
 file_settings: dict = {
     'live_data': {
-        'data_file': 'data/liveData.csv',
-        'diff_file': 'data/differentials/liveDataDiff.csv',
-        'diff2_file': 'data/differentials/liveData2ndOrderDiffFile.csv'
+        'data_file': 'liveData.csv',
+        'diff_file': 'differentials/liveDataDiff.csv',
+        'diff2_file': 'differentials/liveData2ndOrderDiffFile.csv'
     },
     'next_48_hours': {
-        'data_file': 'data/next48Data.csv',
-        'diff_file': 'data/differentials/next48HrsDataDiffFile.csv'
+        'data_file': 'next48Data.csv',
+        'diff_file': 'differentials/next48HrsDataDiffFile.csv'
     },
     'next_7_days': {
-        'data_file': 'data/next7DaysData.csv',
-        'diff_file': 'data/differentials/next7DaysDataDiffFile.csv'
+        'data_file': 'next7DaysData.csv',
+        'diff_file': 'differentials/next7DaysDataDiffFile.csv'
     }
 }
