4b5e9212c41bd386b5fd847d3cbb1979baab126b
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-02-12 23:50:26 -0500

    feat!(api data): change DarkSky for ClimaCell data
    Change data source for weather data from DarkSky API to
    ClimaCell API due to sunset of DarkSky API access, along
    with the fact that ClimaCell also supplies air quality and
    pollen data
    
    BREAKING CHANGE: air_collect prior to this change with depend
    on the DarkSky api data source for weather data.  With this change
    ClimaCell api is used instead and no the DarkSky api is not used
    for any data.

--- air_collect/bin/air_settings.py
+++ air_collect/bin/air_settings.py
@@ -1,17 +1,12 @@
 #  Configuration for different plotting perspectives
 
+UNITS = 'imperial'
+TIMEZONE = 'America/New_York'
 file_settings: dict = {
     'live_data': {
-        'data_file': 'liveData.csv',
-        'diff_file': 'differentials/liveDataDiff.csv',
-        'diff2_file': 'differentials/liveData2ndOrderDiffFile.csv'
+        'data_file': 'liveData.csv'
     },
-    'next_48_hours': {
-        'data_file': 'next48Data.csv',
-        'diff_file': 'differentials/next48HrsDataDiffFile.csv'
-    },
-    'next_7_days': {
-        'data_file': 'next7DaysData.csv',
-        'diff_file': 'differentials/next7DaysDataDiffFile.csv'
+    'next_days': {
+        'data_file': 'nextDaysData.csv'
     }
 }
